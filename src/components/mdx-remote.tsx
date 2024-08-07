569edcd46bc1901ed0a586dc14d82c7fca1f3055
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2024-08-07 03:24:04 +0000

    chore: Update MDX rendering with lazy loading for images and highlight component

--- src/components/mdx-remote.tsx
+++ src/components/mdx-remote.tsx
@@ -4,22 +4,22 @@ import remarkBreaks from "remark-breaks";
 import { compileMDX } from "next-mdx-remote/rsc";
 import Image from "next/image";
 
-const components = {
-  h1: (props: any) => (
-    <h1 className="text-3xl font-bold text-blue-900" {...props} />
-  ),
-  p: (props: any) => <p className="text-warap whitespace-pre" {...props} />,
-  em: (props: any) => <em className="italic" {...props} />,
+const customComponents = {
+  // eslint-disable-next-line jsx-a11y/alt-text
+  Image: (props: any) => <Image {...props} />,
+  HighLight: (props: any) => <span className="bg-yellow-500/50" {...props} />,
 };
 
 export async function CustomMDX(props: any) {
   const {content,frontmatter}= await compileMDX({
     source: props.source,
     components: {
-      ...components,
-      // eslint-disable-next-line jsx-a11y/alt-text
-      Image: (props: any) => <Image {...props} />,
-      HighLight: (props: any) => <span className="bg-yellow-500/50" {...props} />,
+      h1: (props: any) => (
+        <h1 className="text-3xl font-bold text-blue-900" {...props} />
+      ),
+      p: (props: any) => <p className="text-warap whitespace-pre" {...props} />,
+      em: (props: any) => <em className="italic" {...props} />,
+      ...customComponents,
     },
     options: {
       parseFrontmatter: true,
