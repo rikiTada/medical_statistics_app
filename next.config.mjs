7cc5268ac01da6925dd8cd2da0c852f6806ff98f
rikiTada <riki.tada777@gmail.com>
2024-08-11 10:53:16 +0900

    chore: Update next.config.mjs for Cloudflare Workers setting

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import remarkBreaks from "remark-breaks";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: `export`, //cloudflare workers setting
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
