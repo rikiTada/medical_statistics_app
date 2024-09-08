f41508649e49b0dce6af7b719ca560137af5130c
rikiTada <riki.tada777@gmail.com>
2024-09-08 11:18:09 +0900

    Refactor drizzle.config.ts and src/hooks/swr/article.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./db/schema.ts",
-  out: "./drizzle/migrations",
-  dialect: "sqlite",
-  // driver: "d1-http",
-  // dbCredentials: {
-  //   wranglerConfigPath: "wrangler.toml",
-  // },
-} satisfies Config;
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
