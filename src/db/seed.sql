db453982fe9839c554c3e5c91c51d86b3b31f2d2
rikiTada <riki.tada777@gmail.com>
2024-09-14 01:33:03 +0900

    example seeder

---
+++ src/db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+	users (name, email, password)
+VALUES
+	('Jone', 'Jone@example.com', 'password1'),
+	('Jene', 'Jene@example.com', 'password2'),
+	('Jane', 'Jane@example.com', 'password3');
