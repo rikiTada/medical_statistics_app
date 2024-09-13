a10fc71afe5c92ac01e2fca52d5bde8f1c8122b4
rikiTada <riki.tada777@gmail.com>
2024-09-14 01:41:19 +0900

    update: testPage for users

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,36 @@
-import { getArticleTable } from "@/db/service";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { format } from "date-fns";
 
-export default async function Page() {
-  const data = await getArticleTable();
+export default async function Page({
+  params,
+}: {
+  params: { user: string };
+}): Promise<JSX.Element> {
+  // const data = await getArticleTable();
 
-  console.log("Results", data);
-  // http://localhost:3000 を入れないとエラーになる
-  // const data = await fetch("http://localhost:3000/api").then((res) =>
-  //   res.json()
-  // );
+  const data = await db.select().from(users);
 
   return (
-    <>
-      <span>テスト用ページ</span>
-    </>
+    <div>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>createdAt</th>
+          <th>updateAt</th>
+        </tr>
+        {data.map((d) => (
+          <tr key={d.id}>
+            <td>{d.id}</td>
+            <td>{d.name}</td>
+            <td>{d.email}</td>
+            <td>{format(d.createdAt, "yyyy-MM-dd HH:mm:ss")}</td>
+            <td>{format(d.updateAt, "yyyy-MM-dd HH:mm:ss")}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
