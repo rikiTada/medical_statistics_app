ee6666894ad4339927199c44cad07d18729d3bdf
rikiTada <riki.tada777@gmail.com>
2024-09-14 20:18:43 +0900

    ざっくりリファクタ

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,35 +1,71 @@
 import "@/components/sidebar.scss";
 import { db } from "@/db";
 import { articles } from "@/db/schema";
-import { dbGetArticle } from "@/lib/mdx";
 import { cn } from "@/lib/utils";
 import { Home } from "lucide-react";
 import Link from "next/link";
 
-type SidebarProps = Readonly<{
+type SidebarProps = {
   className?: string;
-}>;
+};
+
+type SidebarLinkProps = {
+  href: string;
+  title: string;
+};
+
+type LinkWithCategory = {
+  category: string;
+  items: {
+    href: string;
+    title: string;
+  }[];
+};
 
 export default async function Sidebar({ className }: SidebarProps) {
-  const fs = await dbGetArticle();
-
   const data = await db.select().from(articles);
 
-  const menuList = [
+  const menuList: LinkWithCategory[] = [
     {
-      group: "記事一覧",
-      items: data?.map((article) => ({
-        link: `/articles/${article.id}`,
-        text: article.title,
+      category: "記事一覧",
+      items: data.map((article) => ({
+        href: `/articles/${article.id}`,
+        title: article.title,
       })),
-      fs: fs // fsはあとで消すやつ
-        ?.filter((fs) => fs.meta.title)
-        .map((article) => ({
-          link: `/articles/${article.slug}`,
-          text: article.meta.title,
-        })),
     },
   ];
+
+  const HomeLink = () => (
+    <Link
+      href="/"
+      className="font-medium text-card flex gap-2 items-center p-2 my-2 rounded hover:bg-blue-950"
+    >
+      <Home size={16} />
+      <span>HOME</span>
+    </Link>
+  );
+
+  const SidebarCategory = ({ category, items }: LinkWithCategory) => {
+    return (
+      <div key={category} className="py-2">
+        <span className="text-xs my-2">{category}</span>
+        {items && items.map((item) => <SidebarLink {...item} />)}
+      </div>
+    );
+  };
+
+  const SidebarLink = ({ href, title }: SidebarLinkProps) => {
+    return (
+      <div
+        key={href}
+        className="p-1 w-full hover:bg-blue-950 hover:text-primary-background rounded"
+      >
+        <Link href={href} className="text-base ">
+          <span className="truncate block">- {title}</span>
+        </Link>
+      </div>
+    );
+  };
 
   return (
     <div
@@ -38,59 +74,17 @@ export default async function Sidebar({ className }: SidebarProps) {
         "sidebar bg-blue-900 p-4 min-w-60 overflow-y-auto no-scrollbar"
       )}
     >
-      <Link
-        href="/"
-        className="text-card flex gap-2 items-center py-2 px-2 my-2 rounded hover:bg-blue-950"
-      >
-        <Home color="white" />
-        <span>HOME</span>
-      </Link>
+      <HomeLink />
 
       <hr />
 
       <div className="flex h-full w-full flex-col overflow-hidden rounded-none text-card">
-        <div className="overflow-hidden px-2 py-1.5 ">
+        <div className="overflow-hidden px-2 py-1.5">
           {menuList.map((menu) => (
-            <div key={menu.group} className="py-2">
-              <span className="text-xs my-2">{menu.group}</span>
-              {menu.items &&
-                menu.items.map((item) => (
-                  <div
-                    key={item.link}
-                    className="p-1 w-full hover:bg-blue-950 hover:text-primary-background rounded"
-                  >
-                    <Link href={item.link} className="text-base ">
-                      <span className="truncate block">- {item.text}</span>
-                    </Link>
-                  </div>
-                ))}
-
-              {/*  fsはあとで消すやつ  */}
-              {menu.fs &&
-                menu.fs.map((item) => (
-                  <div
-                    key={item.link}
-                    className="p-1 w-full hover:bg-blue-950 hover:text-primary-background rounded"
-                  >
-                    <Link href={item.link} className="text-base ">
-                      <span className="truncate block">- {item.text}</span>
-                    </Link>
-                  </div>
-                ))}
-              {/* ================ */}
-            </div>
+            <SidebarCategory {...menu} />
           ))}
         </div>
       </div>
-
-      {/* <div className="absolute bottom-0 left-0 w-full h-0 overflow-hidden">
-        <div
-          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-0 h-0
-                          border-l-[20px] border-l-transparent
-                          border-r-[20px] border-r-transparent
-                          border-b-[20px] border-b-white"
-        ></div>
-      </div> */}
     </div>
   );
 }
