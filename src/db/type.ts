e8d7519c166e9520922f3a2f425544d96aec8a59
rikiTada <riki.tada777@gmail.com>
2024-09-14 12:59:14 +0900

    型生成

--- src/db/type.ts
+++ src/db/type.ts
@@ -1,4 +1,46 @@
-import { articles, users } from "@/db/schema";
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import type { SQLiteTableWithColumns } from "drizzle-orm/sqlite-core";
 
-export type User = typeof users.$inferInsert;
-export type Article = typeof articles.$inferInsert;
+import * as schema from "@/db/schema";
+
+// ========================================
+//  schema
+// ========================================
+
+// Users
+export type User = Doc<"users">;
+export type UserInsert = DocInsert<"users">;
+
+// Articles
+export type Article = Doc<"articles">;
+export type ArticleInsert = DocInsert<"articles">;
+
+// ========================================
+//  type generate util  https://soorria.com/snippets/drizzle-type-helpers
+// ========================================
+/**
+ * Filters out any relation definitions from your schema
+ */
+type SchemaTableNames = {
+  [TableOrRelationName in keyof typeof schema]: (typeof schema)[TableOrRelationName] extends SQLiteTableWithColumns<any>
+    ? TableOrRelationName
+    : never;
+}[keyof typeof schema];
+
+type DBSelectTypeMap = {
+  [TableName in SchemaTableNames]: InferSelectModel<(typeof schema)[TableName]>;
+};
+/**
+ * Get the SELECT type for a table given it's export name in the drizzle schema.
+ */
+export type Doc<TableName extends keyof DBSelectTypeMap> =
+  DBSelectTypeMap[TableName];
+
+type DBInsertTypeMap = {
+  [TableName in SchemaTableNames]: InferInsertModel<(typeof schema)[TableName]>;
+};
+/**
+ * Get the INSERT type for a table given it's export name in the drizzle schema.
+ */
+export type DocInsert<TableName extends keyof DBInsertTypeMap> =
+  DBInsertTypeMap[TableName];
