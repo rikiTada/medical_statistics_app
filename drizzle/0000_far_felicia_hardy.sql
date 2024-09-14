eb8726078b050e28e8d3ef5c75679d1799d2830e
rikiTada <riki.tada777@gmail.com>
2024-09-14 18:24:57 +0900

    Refactor database table structure

--- drizzle/0000_far_felicia_hardy.sql
+++ drizzle/0000_far_felicia_hardy.sql
@@ -2,20 +2,21 @@ CREATE TABLE IF NOT EXISTS "articles" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"title" text NOT NULL,
 	"content" text NOT NULL,
-	"tag" text NOT NULL,
-	"group" text NOT NULL,
-	"bookmark" boolean NOT NULL,
-	"content_url" text NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updateAt" timestamp DEFAULT now() NOT NULL
+	"tag" text,
+	"category" text,
+	"bookmark" boolean DEFAULT false NOT NULL,
+	"content_url" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"update_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "folders" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"folder_name" text NOT NULL,
-	"view_order" text NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updateAt" timestamp DEFAULT now() NOT NULL
+	"view_order" text,
+	"article_count" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"update_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
@@ -23,8 +24,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"password" text NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updateAt" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"update_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
