1f89675177395ffcb43e714d7fefbe3a186abcdb
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2024-09-19 04:04:23 +0000

    linkの追加

--- src/app/(admin)/admin/article/edit/page.tsx
+++ src/app/(admin)/admin/article/edit/page.tsx
@@ -5,56 +5,26 @@ import { Input } from "@/components/ui/input";
 import { ArticleInsert } from "@/db/type";
 import { createArticle } from "./actions";
 
+type ExtendsArticleInsert = Pick<ArticleInsert, "title" | "content">;
+
 export default async function Page() {
-  const insertData: ArticleInsert = {
+  const insertData: ExtendsArticleInsert = {
     title: "title1",
     content: "content1",
   };
 
   const renderInput = (
-    key: keyof ArticleInsert,
-    value: ArticleInsert[keyof ArticleInsert]
+    key: keyof ExtendsArticleInsert,
+    value: ExtendsArticleInsert[keyof ExtendsArticleInsert]
   ) => {
-    switch (typeof value) {
-      case "boolean":
-        return (
-          <Input
-            type="checkbox"
-            name={key}
-            id={key}
-            defaultChecked={value}
-            className="rounded"
-          />
-        );
-      case "object":
-        if (value instanceof Date) {
-          return (
-            <Input
-              type="datetime-local"
-              name={key}
-              id={key}
-              defaultValue={value.toISOString().slice(0, 16)}
-              className="w-full rounded"
-            />
-          );
-        }
-        return null;
-      default:
-        return (
-          <Input
-            type="text"
-            name={key}
-            id={key}
-            defaultValue={value || ""}
-            className="w-full rounded"
-          />
-        );
-    }
+    return (
+      <Input type="text" name={key} id={key} defaultValue={value || ""} className="w-full rounded" />
+    );
   };
 
   return (
     <form action={createArticle} className="grid gap-2 m-8">
-      {(Object.keys(insertData) as Array<keyof ArticleInsert>).map((key) => (
+      {(Object.keys(insertData) as Array<keyof ExtendsArticleInsert>).map((key) => (
         <div key={key}>
           <label htmlFor={key} className="block mb-1">
             {key}:
--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <div>admin page</div>;
+  return (
+    <>
+      <h1 className="my-4 text-lg">admin page</h1>
+      <Link href="/admin/article">記事管理</Link>
+      <Link href="/admin/user">ユーザ管理 </Link>
+    </>
+  );
 }
