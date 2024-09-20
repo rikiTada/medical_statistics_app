00117651a1c299d58511a56bb46cd2d4fd54f9c9
rikiTada <riki.tada777@gmail.com>
2024-09-21 01:56:53 +0900

    Add docker-compose commands for starting and stopping the database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,21 @@
+import { sql } from "@vercel/postgres";
 import { config } from "dotenv";
+import { drizzle as drizzle_pg } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import postgres from "postgres";
 
 config({ path: ".env.local" });
 
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+let db;
 
-export const db = drizzle(sql);
+if (process.env.NODE_ENV === "development") {
+  const connectionString = process.env.POSTGRES_URL_LOCAL;
+  if (!connectionString) throw new Error("POSTGRES_URL_LOCAL is not defined");
+
+  const client = postgres(connectionString);
+  db = drizzle_pg(client);
+} else {
+  db = drizzle(sql);
+}
+
+export { db };
