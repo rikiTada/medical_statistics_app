bca9f5df4634bfcd6b84eaaedbf7b6262ff3f70c
rikiTada <riki.tada777@gmail.com>
2024-09-21 08:58:14 +0900

    rm: local pg.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,28 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { config } from "dotenv";
-import {
-  drizzle as drizzlePg,
-  PostgresJsDatabase,
-} from "drizzle-orm/postgres-js";
-import { drizzle, VercelPgDatabase } from "drizzle-orm/vercel-postgres";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 config({ path: ".env.local" });
 
-type Database = VercelPgDatabase | PostgresJsDatabase;
-
-const createDevDatabase = (): PostgresJsDatabase => {
-  const connectionString = process.env.POSTGRES_URL_LOCAL;
-  if (!connectionString) {
-    throw new Error("POSTGRES_URL_LOCAL is not defined");
-  }
-  const client = postgres(connectionString);
-  return drizzlePg(client);
-};
-
-const createProdDatabase = (): VercelPgDatabase => drizzle(sql);
-
-export const db: Database =
-  process.env.NODE_ENV === "development"
-    ? createDevDatabase()
-    : createProdDatabase();
+export const db = drizzle(sql);
