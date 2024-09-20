53e3e32e2dd77a6656016fe4e55c0ee3b8839137
rikiTada <riki.tada777@gmail.com>
2024-09-21 08:55:22 +0900

    add: articles seeder

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -4,3 +4,12 @@ VALUES
 	('Jone', 'Jone@example.com', 'password1'),
 	('Jene', 'Jene@example.com', 'password2'),
 	('Jane', 'Jane@example.com', 'password3');
+
+INSERT INTO
+	articles (id, title, content)
+VALUES
+	(
+		'ea4a742c-b511-4f87-b3e6-0e8477b7b1aa.mdx',
+		'複数の解析対象集団(有用性)',
+		'複数の解析対象集団(有用性)についてわかりやすくまとめ📡',
+	)
